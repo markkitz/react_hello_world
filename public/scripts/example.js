5ec9de8441b7de1c7a580f433844bee802e065a9
markkitz <me@markkitz.com>
2015-11-20 12:01:56 -0700

    try again

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -12,3 +12,24 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+var Timer = React.createClass({
+  getInitialState: function() {
+    return {secondsElapsed: 0};
+  },
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+    );
+  }
+});
+
+ReactDOM.render(<Timer />, document.getElementById('timer'));
